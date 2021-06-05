bdb925c4394729158c3536fa419c1780f8254dc4
Andy <aa.alarcon10@gmail.com>
2021-06-04 22:29:58 -0700

    Added full post view

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -35,6 +35,31 @@
         </div>
       </div>
     </section>
+
+    <!-- full post modal -->
+    <transition name="fade">
+      <div v-if="showPostModal" class="p-modal">
+        <div class="p-container">
+          <a @click="closePostModal()" class="close">close</a>
+          <div class="post">
+            <h5>{{ fullPost.userName }}</h5>
+            <span>{{ formatDate(fullPost.createdOn)}}</span>
+            <p>{{ fullPost.content }}</p>
+            <ul>
+              <li><a>comments {{ fullPost.comments }}</a></li>
+              <li><a>likes {{ fullPost.likes }}</a></li>
+            </ul>
+          </div>
+          <div v-show="postComments.length" class="comments">
+            <div v-for="comment in postComments" :key="comment.id" class="comment">
+              <p>{{ comment.userName }}</p>
+              <span>{{ formatDate(comment.createdOn)}}</span>
+              <p>{{ comment.content }}</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </transition>
   </div>
 </template>
 
@@ -42,6 +67,7 @@
 import { mapState } from 'vuex'
 import moment from 'moment'
 import CommentModal from '@/components/CommentModal'
+import { commentsCollection } from '@/firebase'
 
 export default {
   components: {
@@ -53,7 +79,10 @@ export default {
         content: ''
       },
       showCommentModal: false,
-      selectedPost: {}
+      selectedPost: {},
+      showPostModal: false,
+    fullPost: {},
+    postComments: []
     }
   },
   computed: {
@@ -82,6 +111,20 @@ export default {
     },
     likePost(id, likesCount) {
       this.$store.dispatch('likePost', { id, likesCount })
+    },
+    async viewPost(post) {
+      const docs = await commentsCollection.where('postId', '==', post.id).get()
+      docs.forEach(doc => {
+        let comment = doc.data()
+        comment.id = doc.id
+        this.postComments.push(comment)
+      })
+      this.fullPost = post
+      this.showPostModal = true
+    },
+    closePostModal() {
+      this.postComments = []
+      this.showPostModal = false
     }
     
   }
